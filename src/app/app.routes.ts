0272111235162780eceabdf5a9de33447c2c32f8
karthikeyan.k <k.karthikeyankcbe@gmail.com>
2025-01-09 22:31:34 +0530

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import path from 'path';
+import { CarListComponent } from './components/car-list/car-list.component';
+import { BookingsComponent } from './components/bookings/bookings.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '',redirectTo: 'bookings',pathMatch: 'full'},
+    {path:"car",component:CarListComponent},
+    {path:"bookings",component:BookingsComponent}
+];
